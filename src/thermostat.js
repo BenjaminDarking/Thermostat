7bfda9b79562929381595386a70e9c3848612b54
Ben D <benjamin.d928@icloud.com>
2019-07-24 20:53:35 +0100

    Added temperature up and down

--- src/thermostat.js
+++ src/thermostat.js
@@ -4,6 +4,14 @@ function Thermostat() {
   this.temperature = 20;
 }
 
-Thermostat.prototype.getsCurrentTemperature = function(){
+Thermostat.prototype.getCurrentTemperature = function() {
   return this.temperature;
+};
+
+Thermostat.prototype.up = function(){
+  this.temperature += 1;
+};
+
+Thermostat.prototype.down = function(){
+  this.temperature -= 1;
 };
