70943575566e68aaedef28cf349de28dd41ef5b5
Ben D <benjamin.d928@icloud.com>
2019-07-25 11:42:11 +0100

    added energy usuage feature and tests

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -48,4 +48,58 @@ describe('Thermostat', function(){
     thermostat.switchPowerSavingModeOn();
     expect(thermostat.isPowerSavingModeOn()).toBe(true);
   });
+
+describe('when power saving mode is on', function(){
+  it('has a mximum temperature of 25 degrees', function() {
+    for (var i = 0; i < 6; i++) {
+      thermostat.up();
+    }
+    expect(thermostat.getCurrentTemperature()).toEqual(25);
+  });
+});
+
+describe('when power saving mode is off', function() {
+  it('has a maximum temp of 32 degress', function() {
+    thermostat.switchPowerSavingModeOff();
+    for (var i = 0; i < 13; i++) {
+      thermostat.up()
+    }
+    expect(thermostat.getCurrentTemperature()).toEqual(32);
+  })
+});
+
+it('can be reset to thedefault temperature', function() {
+  for (var i = 0; i < 6; i++) {
+    thermostat.up();
+  }
+  thermostat.resetTemperature();
+  expect(thermostat.getCurrentTemperature()).toEqual(20);
+});
+
+describe('displaying usuage levels', function() {
+  describe('wehn the temperature is below 18 degrees', function() {
+    it('it is considered low useage', function() {
+      for (var i = 0; i < 6; i++) {
+        thermostat.down();
+      }
+      expect(thermostat.energyUsage()).toEqual('low-usage');
+    });
+  });
+
+  describe('when the temperature is between 18 and 25 degrees', function() {
+    it('it is considered medium-usage', function() {
+      expect(thermostat.energyUsage()).toEqual('medium-usage');
+    });
+  });
+
+  describe('when the temperature is anything else', function() {
+    it('it is considered high-usage', function() {
+      thermostat.powerSavingMode = false;
+      for (var i = 0; i < 6; i++) {
+        thermostat.up();
+      }
+      expect(thermostat.energyUsage()).toEqual('high-usage');
+    });
+  });
+});
 });
